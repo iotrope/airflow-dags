c2652690b7e27aea6cdf159ce975eca06ae71224
manan-chauhan <105268788+manan-chauhan@users.noreply.github.com>
2025-05-12 19:06:37 +0530

    daily_etl_coppy_added

---
+++ dags/daily_etl_v2.py
@@ -0,0 +1,26 @@
+# dags/daily_etl.py
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def extract():
+    print("Extracting data...")
+
+def transform():
+    print("Transforming data...")
+
+def load():
+    print("Loading data...")
+
+with DAG(
+    dag_id="daily_etl_pipeline_v2",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["etl"]
+) as dag:
+    t1 = PythonOperator(task_id="extract", python_callable=extract)
+    t2 = PythonOperator(task_id="transform", python_callable=transform)
+    t3 = PythonOperator(task_id="load", python_callable=load)
+
+    t1 >> t2 >> t3
